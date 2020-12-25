8366eed3c04e1d3c4f92b351632138b84c614fc1
RNGSSB <59853617+RNGSSB@users.noreply.github.com>
2020-12-25 12:51:48 -0600

    New tech
    -Edge slipoffs on platforms
    -Skid cancel shield grab
    -Shield dropping
    -Infinite air dodge spam?

--- Edits/src/custom/mod.rs
+++ Edits/src/custom/mod.rs
@@ -1,15 +1,19 @@
+#![warn(non_snake_case)]
 use smash::lib::{L2CValue, L2CAgent};
+use skyline::nro::{self, NroInfo};
+use smash::app::BattleObjectModuleAccessor;
 use smash::app::lua_bind::*;
 use smash::hash40;
 use smash::app::utility::get_kind;
+use smash::app::utility::*;
 use smash::lua2cpp::{L2CFighterCommon, L2CFighterBase};
 use smash::lib::lua_const::*;
 use smash::phx::*;
+use smash::app::GroundCorrectKind;
 use acmd;
 use skyline::nn::ro::LookupSymbol;
 
 static mut LOCKED: [bool; 9] = [false; 9];
-static mut CANAIRDODGE: [bool; 9] = [true; 9];
 static mut LEDGE_POS: [Vector3f; 9] = [smash::phx::Vector3f { x: 0.0, y: 0.0, z: 0.0}; 9];
 
 // Use this for general per-frame fighter-level hooks
@@ -37,12 +41,24 @@ pub fn once_per_fighter_frame(fighter : &mut L2CFighterCommon) {
         removeSHMacro(boma, status_kind);
         quickAttackCancels(boma, status_kind, situation_kind, fighter_kind, stick_value_y);  
         randomTripping(boma, status_kind);
- 
+        skidCancelShieldGrab(boma, status_kind, cat1);
+        shieldDrops(boma, status_kind, cat2);
     }
 }
 
 pub unsafe fn get_player_number(boma: &mut smash::app::BattleObjectModuleAccessor) -> usize {
     return WorkModule::get_int(boma, *FIGHTER_INSTANCE_WORK_ID_INT_ENTRY_ID) as usize;
+}
+
+#[skyline::hook(replace = smash::app::lua_bind::WorkModule::is_enable_transition_term)]
+
+pub unsafe fn is_enable_transition_term_hook(boma: &mut smash::app::BattleObjectModuleAccessor, flag: i32) -> bool {
+    let status_kind = StatusModule::status_kind(boma);
+    let fighter_kind = get_kind(boma);
+    if flag == *FIGHTER_STATUS_TRANSITION_TERM_ID_CONT_ESCAPE_AIR {
+                return true;
+    }
+    original!()(boma, flag)
 }
 
 //DACUS
@@ -97,6 +113,28 @@ pub unsafe fn sm4shJabLocks(boma: &mut smash::app::BattleObjectModuleAccessor, s
     }
 }
 
+//Cancel runBrake with shield
+pub unsafe fn skidCancelShieldGrab(boma: &mut smash::app::BattleObjectModuleAccessor, status_kind: i32, cat1: i32) { //Shield Stop
+    if status_kind == *FIGHTER_STATUS_KIND_RUN_BRAKE {
+        if (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_ESCAPE) != 0 || ControlModule::check_button_trigger(boma, *CONTROL_PAD_BUTTON_GUARD) {
+            StatusModule::change_status_request_from_script(boma, *FIGHTER_STATUS_KIND_GUARD_ON, true);
+        }
+    }
+}
+
+//Shield drops
+pub unsafe fn shieldDrops(boma: &mut smash::app::BattleObjectModuleAccessor, status_kind: i32, cat2: i32) {
+    if status_kind == *FIGHTER_STATUS_KIND_GUARD || status_kind == *FIGHTER_STATUS_KIND_GUARD_ON { //Shield Drop
+        if (cat2 & *FIGHTER_PAD_CMD_CAT2_FLAG_GUARD_TO_PASS) != 0 || (cat2 & *FIGHTER_PAD_CMD_CAT2_FLAG_APPEAL_HI) != 0 || (cat2 & *FIGHTER_PAD_CMD_CAT2_FLAG_APPEAL_LW) != 0  ||
+        (cat2 & *FIGHTER_PAD_CMD_CAT2_FLAG_APPEAL_S_L) != 0 || (cat2 & *FIGHTER_PAD_CMD_CAT2_FLAG_APPEAL_S_R) != 0 {
+            if GroundModule::is_passable_ground(boma) {
+                StatusModule::change_status_request_from_script(boma, *FIGHTER_STATUS_KIND_PASS, true);
+            }
+        }
+    }
+}
+
+
 //Remove Shorthop Attack Macro
 pub unsafe fn removeSHMacro(boma: &mut smash::app::BattleObjectModuleAccessor, status_kind: i32) {
     if status_kind == *FIGHTER_STATUS_KIND_JUMP_SQUAT || StatusModule::prev_status_kind(boma, 0) == *FIGHTER_STATUS_KIND_JUMP_SQUAT 
@@ -137,7 +175,7 @@ pub unsafe fn quickAttackCancels (boma: &mut smash::app::BattleObjectModuleAcces
 //Random tripping
 pub unsafe fn randomTripping (boma: &mut smash::app::BattleObjectModuleAccessor, status_kind: i32) {
     if [*FIGHTER_STATUS_KIND_DASH, *FIGHTER_STATUS_KIND_TURN_DASH].contains(&status_kind) {
-        let rng = smash::app::sv_math::rand(0, 100);
+        let rng = smash::app::sv_math::rand(0, 200);
         if rng == 0 {
             StatusModule::change_status_request_from_script(boma, *FIGHTER_STATUS_KIND_SLIP, true);
         }
@@ -148,7 +186,7 @@ pub unsafe fn randomTripping (boma: &mut smash::app::BattleObjectModuleAccessor,
     }
 
     if [*FIGHTER_STATUS_KIND_TURN_RUN].contains(&status_kind) {
-        let rng = smash::app::sv_math::rand(0, 80);
+        let rng = smash::app::sv_math::rand(0, 180);
         if rng == 0 {
             StatusModule::change_status_request_from_script(boma, *FIGHTER_STATUS_KIND_SLIP, true);
         }
@@ -203,10 +241,85 @@ pub unsafe fn can_entry_cliff_hook(boma: &mut smash::app::BattleObjectModuleAcce
     original!()(boma)
 } 
 
+//=================================================================
+//== StatusModule::init_settings
+//=================================================================
+#[skyline::hook(replace=StatusModule::init_settings)]
+unsafe fn init_settings_hook(boma: &mut BattleObjectModuleAccessor, situation: smash::app::SituationKind, arg3: i32, arg4: u32,
+                             ground_cliff_check_kind: smash::app::GroundCliffCheckKind, arg6: bool,
+                             arg7: i32, arg8: i32, arg9: i32, arg10: i32) -> u64 {
+    let category = get_category(boma);
+    let fighter_kind = get_kind(boma);
+    let status_kind = StatusModule::status_kind(boma);
+    let situation_kind = StatusModule::situation_kind(boma);
+    //
+    // Call edge_slippoffs init_settings
+    let fix = init_settings_edges(boma, situation, arg3, arg4, ground_cliff_check_kind, arg6, arg7, arg8, arg9, arg10);
+
+    original!()(boma, situation, arg3, fix, ground_cliff_check_kind, arg6, arg7, arg8, arg9, arg10)
+}
+
+//=================================================================
+//== init_settings for edge_slipoffs module
+//== Note: This is called from init_settings::init_settings_hook
+//== Note: Forces GroundModule::correct to be called for
+//         certain statuses
+//== Note: JostleModule::set_team(boma, 0) is for walking through
+//         other fighters
+//=================================================================
+pub unsafe fn init_settings_edges(boma: &mut BattleObjectModuleAccessor, situation: smash::app::SituationKind, arg3: i32, arg4: u32,
+                              ground_cliff_check_kind: smash::app::GroundCliffCheckKind, arg6: bool,
+                              arg7: i32, arg8: i32, arg9: i32, arg10: i32) -> u32 {
+    /* "fix" forces GroundModule::correct to be called for the statuses we need */
+    let mut fix = arg4;
+    let category = get_category(boma);
+    let fighter_kind = get_kind(boma);
+    let status_kind = StatusModule::status_kind(boma);
+
+    if category == *BATTLE_OBJECT_CATEGORY_FIGHTER {
+
+        if [*FIGHTER_STATUS_KIND_WAIT,
+            *FIGHTER_STATUS_KIND_GUARD_DAMAGE,
+            *FIGHTER_STATUS_KIND_ESCAPE_AIR_SLIDE,
+            *FIGHTER_STATUS_KIND_DAMAGE].contains(&status_kind) {
+            fix = *GROUND_CORRECT_KIND_GROUND as u32;
+        }
+    }
+    return fix
+}
+
+//=================================================================
+//== GroundModule::correct
+//== Note: This is the "can edge slippoff" function in Smash
+//=================================================================
+#[skyline::hook(replace=GroundModule::correct)]
+unsafe fn correct_hook(boma: &mut BattleObjectModuleAccessor, kind: GroundCorrectKind) -> u64 {
+    let status_kind = StatusModule::status_kind(boma);
+    let situation_kind = StatusModule::situation_kind(boma);
+    let fighter_kind = get_kind(boma);
+    let category = get_category(boma);
+
+    if category == *BATTLE_OBJECT_CATEGORY_FIGHTER {
+        if [*FIGHTER_STATUS_KIND_ESCAPE_AIR,
+            *FIGHTER_STATUS_KIND_LANDING,
+            *FIGHTER_STATUS_KIND_TURN_DASH,
+            *FIGHTER_STATUS_KIND_DASH,
+            *FIGHTER_STATUS_KIND_LANDING_FALL_SPECIAL].contains(&status_kind) {
+            return original!()(boma, GroundCorrectKind(*GROUND_CORRECT_KIND_GROUND));
+        }
+    }
+    original!()(boma, kind)
+}
+
 pub fn install() {
     acmd::add_custom_hooks!(once_per_fighter_frame);
     acmd::add_custom_weapon_hooks!(once_per_weapon_frame);
     skyline::install_hook!(entry_cliff_hook);
     skyline::install_hook!(leave_cliff_hook);
     skyline::install_hook!(can_entry_cliff_hook); 
+    skyline::install_hook!(is_enable_transition_term_hook);
+    skyline::install_hooks!(
+        init_settings_hook,
+		correct_hook,
+    );
 }
